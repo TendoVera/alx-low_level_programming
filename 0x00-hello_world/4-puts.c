1a8dc3b4a1fddbef0204e42c4b09d80afd146a6f
TendoVera <rutsvovera@gmail.com>
2023-02-16 11:44:56 -0800

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Programming is like building a multilingual puzzle, followed by a newline.
+ * 
+ * Return Always 0
+ */
+int main (void){
+	puts("\"Programming is like building a multiligual puzzle");
+	return (0);}
