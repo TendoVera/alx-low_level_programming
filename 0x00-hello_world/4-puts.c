b2d37aa06b87b406450cc42e24d846ec127339c5
TendoVera <rutsvovera@gmail.com>
2023-02-17 09:12:03 -0800

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,12 @@
 
 /**
  * main - Entry point 
- * 
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multiligual puzzle,");
+	puts("\"Programming is like building a multiligual puzzle");
 	return (0);
 }
 
