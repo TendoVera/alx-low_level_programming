d4bb65c8f0d7cf08575642d6db1f6eb0f1baf2fd
TendoVera <rutsvovera@gmail.com>
2023-02-17 09:14:27 -0800

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Entry 
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multiligual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
