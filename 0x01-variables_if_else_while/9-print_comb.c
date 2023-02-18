a3a47b981d2c30828d60678f34ec9891d86924d0
TendoVera <rutsvovera@gmail.com>
2023-02-18 06:54:07 -0800

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int y;
+
+	for (y = 48; y < 58; y++)
+		putchar(y);
+	if (y != 57)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
