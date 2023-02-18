20c831f5bc7ee1a3e938b2268b40e2106dc3525f
TendoVera <rutsvovera@gmail.com>
2023-02-17 17:06:53 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Aways 0
+ */
+
+int main(void)
+{
+	char c;
+	
+	char m;
+
+	c = 'a';
+	m = 'A';
+	while 
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(m <= 'z'){
+			putchar(c);
+			m++;
+		}
+	return (0);
+}
