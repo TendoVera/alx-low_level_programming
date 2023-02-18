d19ca95f9bfb739d28b2d0b96db00fa679f899b0
TendoVera <rutsvovera@gmail.com>
2023-02-17 17:10:12 -0800

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,18 +9,18 @@
 int main(void)
 {
 	char c;
-	
 	char m;
 
 	c = 'a';
 	m = 'A';
+
 	while 
 		(c <= 'z') {
 			putchar(c);
 			c++;
 		}
 	while
-		(m <= 'z'){
+		(m <= 'z') {
 			putchar(c);
 			m++;
 		}
