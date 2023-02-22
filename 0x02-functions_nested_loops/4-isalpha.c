ff09ed0e39326feecf1673acbbaa18b4d37070fb
TendoVera <rutsvovera@gmail.com>
2023-02-22 15:54:34 -0800

    isaplha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,13 @@
 
 /**
  * _isalpha - check the alphabet char
- * @c; characters to be checked
+ * @c: characters to be checked
  * Return: 1 for alphabet character or 0 for any other
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c<= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
