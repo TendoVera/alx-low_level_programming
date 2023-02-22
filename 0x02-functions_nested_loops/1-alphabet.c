6fe1861084c2183ba4f5c79df87ab93be5c7d80f
TendoVera <rutsvovera@gmail.com>
2023-02-22 13:42:29 -0800

    1alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print _alphabet - put alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char m;
+
+	for (m = 'a'; m <= 'z'; m++)
+	{
+		_putchar(m);
+	}
+	_putchar('\n');
+}
