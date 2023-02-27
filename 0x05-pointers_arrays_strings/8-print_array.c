766e298455b4f403d5dca3a7be720e74a8789648
TendoVera <rutsvovera@gmail.com>
2023-02-27 11:14:14 -0800

    printarray

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - print arrays of number
+ * @a: array of int
+ * @n: number of elements
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int h;
+
+	for (h = 0; h < n; h++)
+	{
+		printf("$d", a[h]);
+		if (h != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
