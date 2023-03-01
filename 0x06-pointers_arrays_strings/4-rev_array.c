7fc3a3961e7cf1ef1388c0f2c6052e73215e371f
TendoVera <rutsvovera@gmail.com>
2023-03-01 08:32:07 -0800

    rev array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#inlude "main.h"
+
+/**
+ * reverse_array - reverses the content of a array
+ * @a: array
+ * @n: number of element in array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int m;
+	int h;
+
+	for (m = 0; m < n--; m++)
+	{
+		h = a[m];
+		a[m] = a[n];
+		a[n] = h;
+	}
+}
