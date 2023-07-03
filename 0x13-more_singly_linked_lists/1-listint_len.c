4c3aef911b0dfeff9aad6c5b810149fe3ee037db
Rutendo <rutsvovera@gmail.com>
2023-07-03 09:50:21 -0700

    intnodelen

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,17 +4,17 @@
 
 /**
  * listint_len -  returns the number of elements in a linked listint_t list.
- * @h: elements to be printed.
+ * @h: list.
  *
- *Return: number of nodes.
+ *Return: the number of nodes.
  */
 size_t listint_len(const listint_t *h)
 {
-size_t num = 0;
+size_t node = 0;
 while (h)
 {
-num++;
+node++;
 h = h->next;
 }
-return (num);
+return (node);
 }
