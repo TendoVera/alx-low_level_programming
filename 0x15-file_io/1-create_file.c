1aa9c3bc506e5b00e930cc67273fbd7eab5bb472
Rutendo <rutsvovera@gmail.com>
2023-07-10 12:22:59 -0700

    createfile

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,16 +7,17 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, ltt, w;
+	int fd, ltt, w = 0;
 	if (!filename)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (ltt = 0; text_content[ltt];)
-			ltt++;
+		for (w = 0; text_content[w];)
+			w++;
 	}
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1 || w == -1)
+	ltt = write(fd, text_content, w);
+	if (fd == -1 || ltt == -1)
 		return (-1);
 	close (fd);
 	return (1);
