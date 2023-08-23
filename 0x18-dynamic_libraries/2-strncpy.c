96d76e1b66b8d53fc22e39046502d5dcc4352742
TendoVera <rutsvovera@gmail.com>
2023-08-23 16:39:37 -0700

    dynamicoo

---
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: enter value
+ * @src: enter another value
+ * @n: value
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int b;
+
+	b = 0;
+	while (b < n && src[b] != '\0')
+	{
+		dest[b] = src[b];
+		b++;
+	}
+	while (b < n)
+	{
+		dest[b] = '\0';
+		b++;
+	}
+	return (dest);
+}
